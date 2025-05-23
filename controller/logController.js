cae14584fca1a43a6e1dca83de50c800eb9eb498
aj-codess <jagyei339@gmail.com>
2025-05-23 10:39:00 -0700

    progress

--- controller/logController.js
+++ controller/logController.js
@@ -1,6 +1,36 @@
+import logModel from "./../models/logModel.js";
+import logServices from "../services/logServices.js";
+
+const cookieOptions = {
+  expires: new Date(Date.now() + 15 * 60 * 1000),
+  httpOnly: true,
+  sameSite: "strict",
+};
+
+
+const refreshCookieOptions = {
+  expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  httpOnly: true,
+  sameSite: "strict",
+  secure: true,
+};
 
 const createUser=async(req,res)=>{
     try{
+
+        const obj=await logModel.create(req.body);
+
+        if(!obj){
+            return res.status(500).json({status:false,message:"User Not Saved"});
+        }
+
+        const token=await logServices.signToken(obj.userId);
+        const refreshToken=await logServices.signToken(obj.userId);
+
+        res.cookie("authToken",token,cookieOptions);
+        res.cookie("refreshToken",refreshToken,refreshCookieOptions);
+
+        return res.status(200).json({status:true,data:obj,message:"User Created Successfully"});
 
     } catch(error){
         res.status(500).json({message:"Internal Server Error Creating User"});
@@ -11,6 +41,24 @@ const createUser=async(req,res)=>{
 
 const loginUser=async(req,res)=>{
     try{
+
+        const obj=await logModel.login(req.body);
+
+        if(!obj){
+            return res.status(500).json({status:false,message:"Internal Server Error"});
+        }
+
+        if(!obj.status){
+            return res.status(203).json(obj);
+        };
+
+        const token=await logServices.signToken(obj.userId);
+        const refreshToken=await logServices.signToken(obj.userId);
+
+        res.cookie("authToken",token,cookieOptions);
+        res.cookie("refreshToken",refreshToken,refreshCookieOptions);
+
+        return res.status(200).json(obj);
 
     } catch(error){
         res.status(500).json({message:"Internal Server Error in Loggin"});
@@ -23,11 +71,19 @@ const loginUser=async(req,res)=>{
 const logoutUser=async(req,res)=>{
     try{
 
+        res.clearCookie("authToken",{
+            httpOnly: true,
+            sameSite: 'Strict',
+            secure: true,
+        });
+
+        return res.status(200).json({status:true,message:"Logged out successfully"})
+
     } catch(error){
         res.status(500).json({message:"Internal Server Error logging out user"});
         console.log("Error in Logout - ",error);
     }
-}
+};
 
 
 export default {
--- models/logModel.js
+++ models/logModel.js
@@ -1,12 +1,21 @@
+import bcrypt from "bcrypt";
 import db from "./../config/db.js";
 import utility from "./../services/utility.js";
+import logServices from "./../services/logServices.js";
 
 const create=async(payload)=>{
     try{
         const userId=utility.genId();
-        const {email,password,name,phone}=payload;
+        const {email,password,name}=payload;
 
-        
+        const hashedPassword=logServices.hashedPassword(password);
+
+         const result = await db.client.query(
+            `INSERT INTO users (name, email, password, userId) VALUES ($1, $2, $3, $4) RETURNING id, name, email`,
+            [name, email, hashedPassword, userId]
+        );
+
+        return result.rows[0];
 
     } catch(error){
         console.log("database Error in Creating a User - ",error);
@@ -15,6 +24,38 @@ const create=async(payload)=>{
 };
 
 
+const login=async(payload)=>{
+    try{
+
+        const {email,password,phone}=payload;
+
+        const hashedPassword=logServices.passHash(password);
+
+        const result = await db.client.query(
+            `SELECT * FROM users WHERE email = $1`,
+            [email]
+        );
+
+        const user = result.rows[0];
+            if (!user) {
+                return {status:false,messge:"User Not Found"}
+            };
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+            if (!isPasswordValid) {
+                return {status:false,message:"Incorrect Password"};
+            };
+
+        return {status:true, userId: user.userId, name: user.name, email: user.email };
+
+    } catch(error){
+        console.log("Database Error while logging in");
+        throw error;
+    }
+}
+
+
 export default {
-    create
+    create,
+    login
 }
